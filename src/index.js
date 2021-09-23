f9463acb3de1e5fe60998438633c6f16fbe3ef4f
Eduardo Medeiros <eduardoklechowicz@hotmail.com>
2021-09-23 00:04:59 -0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,78 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import styled, { keyframes } from "styled-components";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const Mountains = styled.img`
+  position: absolute;
+  width: 750px;
+  left: -77px;
+  bottom: -1090px;
+`;
+
+const contentJudder = keyframes`
+  0% {
+    transform: translateY(0);
+  }
+  70% {
+    transform: translateY(0);;
+  }
+  80% {
+    transform: translateY(-5px);;
+  }
+  90% {
+    transform: translateY(0);
+  }
+`;
+
+const Content = styled.div`
+  animation: ${contentJudder} 1s infinite;
+`;
+
+const godzillaWalk = keyframes`
+  100% {
+    transform: translateX(1080px);
+  }
+`;
+
+const GodzillaWrapper = styled.div`
+  position: absolute;
+  z-index: 5;
+  left: -450px;
+  bottom: -40px;
+  animation:
+    ${godzillaWalk} 15s infinite linear;
+`;
+
+const godzillaStomp = keyframes`
+  0% {
+    transform: rotate(0);
+  }
+  60% {
+    transform: translateY(-13px) rotate(-4deg);
+  }
+  70% {
+    transform: rotate(0);;
+  }
+`;
+
+const Godzilla = styled.img`
+  width: 440px;
+  animation: ${godzillaStomp} 1s infinite;
+`;
+
+const GodzillaComponent = () => (
+  <div>
+    <Content>
+      <Mountains
+        src={require("./images/mountains.png")} />
+    </Content>
+    <GodzillaWrapper>
+      <Godzilla
+        src={require("./images/godzilla.png")} />
+    </GodzillaWrapper>
+  </div>
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+export default GodzillaComponent;
+
+ReactDOM.render(<GodzillaComponent />, document.getElementById('root'))
